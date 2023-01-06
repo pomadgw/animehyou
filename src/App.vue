0a83a1166740ce91774d0147d57aa896ca7f1c1d
Rahadian Yusuf <rahadian.y@gmail.com>
2023-01-06 18:11:33 +0700

    fix: remove more generated files and replace the default app with the site title

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="font-sans">
+    <h1 class="text-5xl">Animehyou</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
