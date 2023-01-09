25f691b385468a282d78e4c8543cfffe24c65960
Rahadian Yusuf <rahadian.y@gmail.com>
2023-01-09 16:41:39 +0700

    tweak entrylist: make the label bold

--- src/components/EntryList.vue
+++ src/components/EntryList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex mb-3 last:mb-0">
-    <div class="mr-3 w-20 md:w-40">{{ props.label }}</div>
+    <div class="font-bold mr-3 w-20 md:w-40">{{ props.label }}</div>
     <div v-if="html" class="flex-1" v-html="props.html" />
     <div v-else class="flex-1">{{ props.value }}</div>
   </div>
