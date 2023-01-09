ce5145e688ca1e1c5f1537699651fb1f5bee68a9
Rahadian Yusuf <rahadian.y@gmail.com>
2023-01-09 13:35:10 +0700

    init add anime detail page

---
+++ src/pages/AnimeDetailPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    {{ props.id }}
+  </div>
+</template>
+<script setup lang="ts">
+// import { getAnimeList } from '../api'
+
+const props = defineProps({ id: String })
+</script>
