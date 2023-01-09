8c830b9a93c9c4dd57dd3856a151ad0f7f0fa701
Rahadian Yusuf <rahadian.y@gmail.com>
2023-01-09 18:23:55 +0700

    add test with vitest
    we use vitest because it is integrated with vite
    and has compatible api with jest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  test: {
+    globals: true, // required
+    setupFiles: ['vitest-localstorage-mock'],
+    mockReset: false
+  }
 })
