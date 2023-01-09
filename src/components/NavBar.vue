4b72fc737d6d47a4231bba9c144e36ad69efe414
Rahadian Yusuf <rahadian.y@gmail.com>
2023-01-09 09:22:36 +0700

    initialize some components

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="py-3 px-6">
+    <div class="text-2xl font-bold">Animehyou</div>
+
+    <ul>
+      <li>
+        <router-link to="/">Homepage</router-link>
+        <!-- TODO: implement bookmark page -->
+        <router-link to="/bookmark">Bookmark</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
