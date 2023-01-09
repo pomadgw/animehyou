9f67a3e3f3277594c9e149e6a2b9801e44a32a71
Rahadian Yusuf <rahadian.y@gmail.com>
2023-01-09 16:33:46 +0700

    implement anime detail page

--- src/pages/AnimeDetailPage.vue
+++ src/pages/AnimeDetailPage.vue
@@ -1,34 +1,31 @@
 <template>
   <div v-if="detail">
-    <div class="flex flex-col">
-      <div>
+    <h1 class="text-3xl font-bold mb-6">{{ detail.title.english }}</h1>
+
+    <div class="flex flex-col md:flex-row">
+      <div class="md:mr-10">
         <img :src="detail.coverImage.extraLarge" :alt="detail.title.english" />
       </div>
 
-      <div>
-        <div>Name</div>
-        <div>{{ detail.title.english }}</div>
+      <div class="mt-6 md:mt-0">
+        <EntryList :value="detail.title.english" label="Name" />
+        <EntryList :html="detail.description" label="Description" />
 
-        <div>Description</div>
-        <div v-html="detail.description" />
-
-        <div>Average score</div>
-        <div>{{ detail.averageScore }}</div>
-
-        <div>Genres</div>
-        <div>{{ detail.genres }}</div>
+        <EntryList :value="`${detail.averageScore}`" label="Average score" />
+        <EntryList :value="detail.genres.join(', ')" label="Genres" />
       </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
+import EntryList from '../components/EntryList.vue'
 import { getAnimeList } from '../api'
 import { Media } from '../types'
 
 const detail = ref<Media | null>(null)
 
-const props = defineProps({ id: String })
+const props = defineProps<{ id: string }>()
 
 onMounted(async () => {
   const page = await getAnimeList({ id: props.id })
