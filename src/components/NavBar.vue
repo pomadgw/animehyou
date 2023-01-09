df1b98c7c7c802d23fa046d851334312a7a7c834
Rahadian Yusuf <rahadian.y@gmail.com>
2023-01-09 11:10:10 +0700

    add icon

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,8 @@
 <template>
   <nav class="py-4 px-6 flex items-center">
+    <div class="mr-3">
+      <img src="/icon.png" alt="Animehyou logo" class="w-12" />
+    </div>
     <div class="text-3xl font-bold">Animehyou</div>
 
     <ul class="flex ml-6">
