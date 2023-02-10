dbea1c5f415df5b978539a08c47bbd847e103654
Rahadian Yusuf <rahadian.y@gmail.com>
2023-02-10 14:02:07 +0700

    format

--- jest.config.js
+++ jest.config.js
@@ -12,9 +12,9 @@ const customJestConfig = {
     // Handle module aliases (this will be automatically configured for you soon)
     '^@/components/(.*)$': '<rootDir>/components/$1',
 
-    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1'
   },
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jest-environment-jsdom'
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
